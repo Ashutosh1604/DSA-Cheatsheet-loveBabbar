ca77eb0a754699d5c442029d2423a01dfd3302bd
Ashutosh_Dhyani <82019070+Ashutosh1604@users.noreply.github.com>
2024-05-09 11:47:23 +0530

    FindTheDuplicate.cpp

--- Array/FindTheDuplicate.cpp
+++ Array/FindTheDuplicate.cpp
@@ -1,5 +1,50 @@
+
 #include<bits/stdc++.h>
 using namespace std;
+
+
+//optimal approach
+//TC-O(n)
+//SC-O(1)
+int main()
+{
+
+     int n;
+    cin>>n;
+
+    int arr[n];
+    for(int i=0;i<n;i++)
+    {
+        cin>>arr[i];
+    }
+
+
+       int slow=arr[0];
+        int fast=arr[0];
+
+        do{
+            slow=arr[slow];
+            fast=arr[arr[fast]];
+
+        }while(slow!=fast);
+
+        fast=arr[0];
+        while(slow!=fast)
+        {
+            slow=arr[slow];
+            fast=arr[fast];
+        }
+
+        cout<<slow;
+    }
+
+
+/*
+
+2nd approach
+//TC- O(n)
+//SC-0(n)
+
 
 int main()
 {
@@ -24,3 +69,6 @@ int main()
         mp[arr[i]]=1;
     }
 }
+
+
+*/
