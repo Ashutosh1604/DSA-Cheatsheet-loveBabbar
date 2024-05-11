233e20910e71164091ded72d82a57a4bce583ce6
Ashutosh_Dhyani <82019070+Ashutosh1604@users.noreply.github.com>
2024-05-11 13:10:47 +0530

    SmallestSubarrayWithSumGreaterThanK.cpp

--- Array/SmallestSubarrayWithSumGreaterThanK.cpp
+++ Array/SmallestSubarrayWithSumGreaterThanK.cpp
@@ -1,7 +1,10 @@
 
+//1st approach
+//TC:- O(n^2)
+
 #include<bits/stdc++.h>
 using namespace std;
-
+/*
 int main()
 {
     int arr[]={1,10,5,2,7};
@@ -19,11 +22,49 @@ int main()
 
            if(sum>K)
            {
-               miSub=min(miSub,j-1+1);  //min of mi or size of subarray
+               miSub=min(miSub,j-i+1);  //min of mi or size of subarray
            }
        }
    }
 
 
    cout<<miSub<<endl;
+}
+
+*/
+//2nd approach
+//TC-O(n)
+
+int main()
+{
+    int arr[]= {1,4,5,2,7};
+    int n=5;
+    int k=9;
+
+    int i=0;
+    int j=0;
+    int sum=0;
+    int len=INT_MAX;
+
+    while(i<=j && j<n)
+    {
+        sum+=arr[j];
+
+        while(sum-arr[i]>k)
+        {
+            sum-=arr[i];
+            i++;
+        }
+
+        if(sum>k)
+        {
+            len=min(len,j-i+1);
+        }
+
+        j++;
+    }
+
+
+
+    cout<<len<<endl;
 }
